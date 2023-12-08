89fd3f393d1e11c716e1aaf02913caa247e11384
Sourav Sinha <4souravsinhain@gmail.com>
2023-12-09 01:38:23 +0530

    delete sno fixed

--- 22. Todo Project/index.js
+++ 22. Todo Project/index.js
@@ -4,9 +4,9 @@ const setNewTaskValue = (e) => {
     console.log(newTaskValue)
 }
 
-function toggleAddTaskButton(e){
+function toggleAddTaskButton(){
     const addTaskButton = document.getElementById("add-task-btn")
-    if(e.target.value){
+    if(newTaskValue){
         addTaskButton.removeAttribute('disabled')
         console.log("enabled")
     }else{
@@ -20,13 +20,23 @@ const updateInputField = ()=>{
     inputField.value = newTaskValue
 }
 
+function deleteHandle(e){
+    const rowToDelete = e.target.parentNode.parentNode;
+    rowToDelete.remove();
+    const todoData = document.getElementById('todo-data');
+    const todoRows = todoData.getElementsByClassName('todo-row');
+    for(let i=0;i<todoRows.length;i++){
+        todoRows[i].getElementsByClassName('todo-no')[0].innerHTML = i+1;
+    }
+}
+
 const addTasks = (e)=>{
     if(newTaskValue=="") return;
     const parentDiv = document.getElementById("todo-data")
     const newelement = document.createElement('div');
     const totalTasks = document.getElementsByClassName('todo-no').length
     newelement.innerHTML = `
-    <div class="flex justify-between items-center border-l-2 border-r-2 border-b-2 border-gray-300 p-2 mt-0 space-x-4 list-todo-heading h-full">
+    <div class="flex justify-between items-center border-l-2 border-r-2 border-b-2 border-gray-300 p-2 mt-0 space-x-4 h-full todo-row">
         <div class="basis-1/6 text-center text-xl border-r-2 border-gray-300 todo-no">
             ${totalTasks}
         </div>
@@ -37,7 +47,7 @@ const addTasks = (e)=>{
             In progress
         </div>
         <div class="basis-1/6 text-center text-xl todo-action">
-            <button class="border-2 border-red-500 bg-red-500 text-white p-1 rounded-lg hover:bg-red-700">
+            <button class="border-2 border-red-500 bg-red-500 text-white p-1 rounded-lg hover:bg-red-700" onclick="deleteHandle(event)">
                 Delete
             </button>
             <button class="border-2 border-green-500 bg-green-500 text-white p-1 rounded-lg hover:bg-green-700 ml-2">
@@ -45,9 +55,9 @@ const addTasks = (e)=>{
             </button>
         </div>
     </div>`;
+    parentDiv.appendChild(newelement) 
     newTaskValue=""
     updateInputField()
-    // toggleAddTaskButton()
-    parentDiv.appendChild(newelement) 
+    toggleAddTaskButton()
     console.log("done")
 }
