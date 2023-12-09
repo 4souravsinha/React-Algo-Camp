9f59043483ff16559b66f26a3739ff80448a34d7
Sourav Sinha <4souravsinhain@gmail.com>
2023-12-09 23:48:00 +0530

    made the paddle move

--- 23. Ping Pong Game/index.js
+++ 23. Ping Pong Game/index.js
@@ -18,8 +18,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
         ballStyle.left = `${ballX}px`;
         ballStyle.top = `${ballY}px`;
-        
+
         if(ballX >= playground.offsetWidth - ball.offsetWidth|| ballX <= 0) dx *= -1;
         if(ballY >= playground.offsetHeight - ball.offsetHeight || ballY <= 0) dy *= -1;
     }, 1);
+
+    let paddle = document.getElementById('paddle');
+    let paddleY = 0;
+    let pDy = 10;
+    document.addEventListener("keydown", function(event) {
+        switch(event.key) {
+            case 'ArrowDown':
+                if(paddleY < playground.offsetHeight - paddle.offsetHeight) {
+                    paddleY += pDy;
+                    paddle.style.top = `${paddleY}px`;
+                }
+                break;
+            case 'ArrowUp':
+                if(paddleY > 0) {
+                    paddleY += pDy * -1;
+                    paddle.style.top = `${paddleY}px`;
+                }
+                break;
+        }
+    })
 });
