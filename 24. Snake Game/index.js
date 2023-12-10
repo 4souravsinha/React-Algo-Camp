1e80b772affb16bc36c6de4e3bab0eff93480e70
Sourav Sinha <4souravsinhain@gmail.com>
2023-12-10 11:19:01 +0530

    added scoreboard and start button

--- 24. Snake Game/index.js
+++ 24. Snake Game/index.js
@@ -1,3 +1,42 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
+  const gameArena = document.getElementById("game-arena");
+  const arenaSize = gameArena.offsetWidth - gameArena.style.borderWidth;
+  let score = 0;
+  let gameStarted = false;
+  let food = {
+    x: 300,
+    y: 200,
+  };
+  let snake = [
+    {
+      x: 160,
+      y: 200,
+    },
+    {
+      x: 140,
+      y: 200,
+    },
+    {
+      x: 120,
+      y: 200,
+    },
+  ];
+  let dx = [0 , -20 , 20 , 0 , 0]; 
+  let dy = [0 , 0 , 0 , -20 , 20]; 
+  let direction = 0;
+
+
+  function startGame(){
+    const scoreBoard = document.createElement("div");
+    scoreBoard.id = "score-board";
+    document.body.insertBefore(scoreBoard, gameArena);
+
+    const startButton = document.createElement("button");
+    startButton.id = "start-btn";
+    startButton.innerHTML = "Start Game";
+    document.body.appendChild(startButton);
+
     
+  }
+  startGame();
 });
